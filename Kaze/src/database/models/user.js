d1e812a7d1e01b41aaadf0c564208a39befda317
Kaze <146962684+kazeynx@users.noreply.github.com>
2025-01-05 01:09:16 +0700

    Delete Kaze directory

--- Kaze/src/database/models/user.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const userSchema = new Schema({
-    userId: { type: String, required: true },
-    points: { type: Number, default: 0 },
-    level: { type: Number, default: 1 }
-});
-
-module.exports = model('User', userSchema);
