7a869788023b78e29e4c71daa0de97289e39e2f9
Kaze <146962684+kazeynx@users.noreply.github.com>
2025-01-06 20:36:09 +0700

    Delete handlers/loader directory

--- handlers/loader/handlerloader.js
+++
@@ -1,7 +0,0 @@
-const loadCommands = require('./commandhandler');
-const loadEvents = require('./eventhandler');
-
-module.exports = (client) => {
-  loadCommands(client);
-  loadEvents(client);
-};
